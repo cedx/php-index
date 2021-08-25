e64b7359d65acba1ae439c127aa603e5818cc43b
Cédric Belin <cedric@belin.io>
2021-08-25 18:55:37 +0200

    Updated the stylesheet

--- src/directory_index/client/Navbar.hx
+++ src/directory_index/client/Navbar.hx
@@ -19,19 +19,19 @@ class Navbar extends View {
 	/** Renders this view. **/
 	function render() '
 		<nav class="bg-primary d-print-none navbar navbar-dark navbar-expand-lg fixed-top shadow user-select-none">
-			<div class="d-flex align-items-center justify-content-between">
+			<div class="container-fluid">
 				<div class="d-flex align-items-center">
-					<div class="navbar-brand">
+					<span class="navbar-brand">
 						<img alt="" height=${24} src="?file=img/logo_light.svg" width=${97}/>
-					</div>
-					<div class="navbar-brand">
+					</span>
+					<span class="navbar-brand">
 						${Browser.location.hostname}
-					</div>
+					</span>
 				</div>
 
-				<div class="d-none d-sm-block navbar-text text-capitalize-first">
+				<span class="d-none d-sm-block navbar-text text-capitalize-first">
 					${date}
-				</div>
+				</span>
 			</div>
 		</nav>
 	';
