33754b265cab0106a961106fb61bc36a1f68f1e2
Cédric Belin <cedric@belin.io>
2021-08-29 01:03:52 +0200

    Implements the build of the PHP archive

--- src/directory_index/cli/Program.hx
+++ src/directory_index/cli/Program.hx
@@ -1,10 +1,11 @@
 package directory_index.cli;
 
 import directory_index.base.Version;
-import php.PharData;
+import php.Phar;
 import sys.FileSystem;
 import sys.io.File;
 
+using Lambda;
 using haxe.io.Path;
 
 /** Build the PHP redistributable. **/
@@ -30,21 +31,31 @@ class Program {
 	public function run(): Promise<Noise> {
 		if (help) { Cli.getDoc(this); return Noise; }
 		if (version) { Version.getPackageVersion(); return Noise; }
-		// TODO if (!Phar.canWrite()) return new Error(MethodNotAllowed, "Phar extension does not support creating Phar archives.");
 
+		if (!Phar.canWrite()) return new Error(MethodNotAllowed, "Phar extension does not support creating Phar archives.");
 		buildPhar();
 		return Noise;
 	}
 
-	/** TODO Builds the PHP archive. **/
+	/** Builds the PHP archive. **/
 	function buildPhar() {
 		final buildDir = Path.join([basePath, "var/build"]);
-		for (folder in ["lib", "www"]) copyDirectory(Path.join([basePath, folder]), Path.join([buildDir, folder]));
-		FileSystem.deleteFile(Path.join([buildDir, "www/index.php"]));
+		if (FileSystem.exists(buildDir)) removeDirectory(buildDir);
+		["index.phar", "index.phar.gz"].map(file -> Path.join([basePath, 'var/$file'])).filter(FileSystem.exists).iter(FileSystem.deleteFile);
 
-		final phar = new PharData("var/index.zip");
-		trace(phar.buildFromDirectory(buildDir));
-		//phar.setStub("<?php __HALT_COMPILER();");
+		for (folder in ["lib", "www"]) copyDirectory(Path.join([basePath, folder]), Path.join([buildDir, folder]));
+		["index.phar", "index.php"].map(file -> Path.join([buildDir, 'www/$file'])).filter(FileSystem.exists).iter(FileSystem.deleteFile);
+		["directory_index/cli", "tink/cli"].map(folder -> Path.join([buildDir, 'lib/$folder'])).iter(removeDirectory);
+
+		final phar = new Phar(Path.join([basePath, "var/index.phar"]));
+		phar.buildFromDirectory(buildDir);
+		phar.setStub("<?php __HALT_COMPILER();");
+
+		if (!Phar.canCompress()) File.copy(Path.join([basePath, "var/index.phar"]), Path.join([basePath, "www/index.phar"]));
+		else {
+			phar.compress(Phar.GZ);
+			File.copy(Path.join([basePath, "var/index.phar.gz"]), Path.join([basePath, "www/index.phar"]));
+		}
 	}
 
 	/** Recursively copies all files in the specified `source` directory to a given `destination` directory. **/
@@ -56,5 +67,16 @@ class Program {
 			FileSystem.createDirectory(output.directory());
 			File.copy(input, output);
 		}
+	}
+
+	/** Recursively deletes the specified `directory`. **/
+	function removeDirectory(directory: String) {
+		for (entry in FileSystem.readDirectory(directory)) {
+			final path = Path.join([directory, entry]);
+			if (FileSystem.isDirectory(path)) removeDirectory(path);
+			else FileSystem.deleteFile(path);
+		}
+
+		FileSystem.deleteDirectory(directory);
 	}
 }
