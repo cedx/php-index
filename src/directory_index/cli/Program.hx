cbda86e17a134cdbd4218c1c09e5ed89206bbb78
Cédric Belin <cedric@belin.io>
2021-08-30 17:10:19 +0200

    Finalized the console application

--- src/directory_index/cli/Program.hx
+++ src/directory_index/cli/Program.hx
@@ -1,8 +1,6 @@
 package directory_index.cli;
 
 import directory_index.base.Version;
-// TODO import php.Phar;
-// TODO import php.PharData;
 import sys.FileSystem;
 import sys.io.File;
 import tink.Cli;
@@ -25,78 +23,43 @@ class Program {
 	/** Output the version number. **/
 	public var version = false;
 
-	/** The root directory of this application. **/
-	final basePath = Sys.programPath().directory();
-
 	/** Creates a new program. **/
 	public function new() {}
 
 	/** Application entry point. **/
 	static function main() Cli.process(Sys.args(), new Program()).handle(Cli.exit);
 
-	// Runs this command.
+	/** <directory> : The path to the output directory. **/
 	@:defaultCommand
-	public function run(): Promise<Noise> {
+	public function run(rest: Rest<String>): Promise<Noise> {
 		if (help) { Cli.getDoc(this); return Noise; }
 		if (version) { Version.getPackageVersion(); return Noise; }
 
-		// TODO if (!Phar.canWrite()) return new Error(MethodNotAllowed, "Phar extension does not support creating Phar archives.");
-		buildDataFile();
+		final haxelibRun = Sys.getEnv("HAXELIB_RUN") == "1";
+		final requiredArgs = 1;
+		if (rest.length < requiredArgs || (haxelibRun && rest.length < requiredArgs + 1))
+			return new Error(BadRequest, "You must provide the path of the output directory.");
+
+		final output = rest[0].isAbsolute() ? rest[0] : Path.join([haxelibRun ? rest[rest.length - 1] : Sys.getCwd(), rest[0]]);
+		FileSystem.createDirectory(output);
+		buildDataFile(output);
 		return Noise;
 	}
 
-	/** Builds the data file. **/
-	function buildDataFile() {
-		final buildDir = Path.join([Tools.getTempDirectory(), Uuid.v4()]);
-		for (folder in ["lib", "www"]) Tools.copyDirectory(Path.join([basePath, folder]), Path.join([buildDir, folder]));
+	/** Builds the data file in the specified `destination` directory. **/
+	function buildDataFile(destination: String) {
+		final buildDir = Path.join([Tools.tempDirectory, Uuid.v4()]);
+		final rootDir = Sys.programPath().directory();
+		for (folder in ["lib", "www"]) Tools.copyDirectory(Path.join([rootDir, folder]), Path.join([buildDir, folder]));
 		["index.php", "index.zip"].map(file -> Path.join([buildDir, 'www/$file'])).filter(FileSystem.exists).iter(FileSystem.deleteFile);
 
-		final previousCwd = Sys.getCwd();
+		final workingDirectory = Sys.getCwd();
 		Sys.setCwd(buildDir);
-		Tools.compress(["lib", "www"], Path.join([basePath, 'www/index.zip']), compress ? 9 : 0);
-		Sys.setCwd(previousCwd);
+		Tools.compress(["lib", "www"], Path.join([destination, "index.zip"]), compress ? 9 : 0);
+		Sys.setCwd(workingDirectory);
+
+		final inputFile = Path.join([rootDir, "www/index.php"]);
+		final outputFile = Path.join([destination, "index.php"]);
+		File.saveContent(outputFile, ~/const useDataFile = false/.replace(File.getContent(inputFile), "const useDataFile = true"));
 	}
-
-	/** Builds the data file. **/
-	/*
-	function buildPhar() {
-		final buildDir = Path.join([basePath, "var/build"]);
-		if (FileSystem.exists(buildDir)) Tools.removeDirectory(buildDir);
-		["index.phar", "index.phar.gz"].map(file -> Path.join([basePath, 'var/$file'])).filter(FileSystem.exists).iter(FileSystem.deleteFile);
-
-		for (folder in ["lib", "www"]) Tools.copyDirectory(Path.join([basePath, folder]), Path.join([buildDir, folder]));
-		["index.phar", "index.php"].map(file -> Path.join([buildDir, 'www/$file'])).filter(FileSystem.exists).iter(FileSystem.deleteFile);
-		["directory_index/cli", "tink/cli"].map(folder -> Path.join([buildDir, 'lib/$folder'])).iter(Tools.removeDirectory);
-
-		final phar = new Phar(Path.join([basePath, "var/index.phar"]));
-		phar.buildFromDirectory(buildDir);
-		phar.setStub("<?php __HALT_COMPILER();");
-
-		if (!Phar.canCompress()) File.copy(Path.join([basePath, "var/index.phar"]), Path.join([basePath, "www/index.phar"]));
-		else {
-			phar.compress(Phar.GZ);
-			File.copy(Path.join([basePath, "var/index.phar.gz"]), Path.join([basePath, "www/index.phar"]));
-		}
-	}*/
-
-	/** Builds the data file. **/
-	/*
-	function buildPharData() {
-		final buildDir = Path.join([basePath, "var/build"]);
-		if (FileSystem.exists(buildDir)) Tools.removeDirectory(buildDir);
-		["index.phar", "index.phar.gz"].map(file -> Path.join([basePath, 'var/$file'])).filter(FileSystem.exists).iter(FileSystem.deleteFile);
-
-		for (folder in ["lib", "www"]) Tools.copyDirectory(Path.join([basePath, folder]), Path.join([buildDir, folder]));
-		["index.phar", "index.php"].map(file -> Path.join([buildDir, 'www/$file'])).filter(FileSystem.exists).iter(FileSystem.deleteFile);
-		["directory_index/cli", "tink/cli"].map(folder -> Path.join([buildDir, 'lib/$folder'])).iter(Tools.removeDirectory);
-
-		final phar = new PharData(Path.join([basePath, "var/index.zip"]));
-		phar.buildFromDirectory(buildDir);
-
-		if (!Phar.canCompress()) File.copy(Path.join([basePath, "var/index.zip"]), Path.join([basePath, "www/index.phar"]));
-		else {
-			phar.compressFiles(Phar.GZ);
-			File.copy(Path.join([basePath, "var/index.zip"]), Path.join([basePath, "www/index.phar"]));
-		}
-	}*/
 }
