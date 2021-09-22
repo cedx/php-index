74d02aaaca906f53761368faaeae07fcf5688434
Cédric Belin <cedric@belin.io>
2021-09-22 12:21:16 +0200

    Updated the build system

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -1,12 +1,20 @@
+//! --class-path src
 import Sys.*;
+import php_index.base.Version.*;
 import sys.FileSystem.*;
+import sys.io.File.*;
 
 using Lambda;
 
 /** Runs the script. **/
 function main() {
 	for (script in ["Clean", "Version", "Build"]) command('lix $script');
-	["css/main.css", "js/main.js"].map(file -> 'www/$file.map').filter(exists).iter(deleteFile);
+	["css/main.css", "js/main.js", "worker.js"].map(file -> 'www/$file.map').filter(exists).iter(deleteFile);
+
 	command("npx cleancss -O2 --output=www/css/main.css www/css/main.css");
-	command("npx terser --comments=false --config-file=etc/terser.json --output=www/js/main.js www/js/main.js");
+	for (file in ["js/main.js", "worker.js"]) command('npx terser --comments=false --config-file=etc/terser.json --output=www/$file www/$file');
+
+	final output = append("www/worker.js");
+	output.writeString('\n// ${getGitCommitHash()}');
+	output.close();
 }
