a47fb398ff9a43a867fbf083bf23ae94d2818cbf
Cédric Belin <cedric@belin.io>
2021-09-26 15:40:32 +0200

    Updated the translation

--- src/php_index/client/Locale.hx
+++ src/php_index/client/Locale.hx
@@ -2,8 +2,10 @@ package php_index.client;
 
 /** Provides the localization table. **/
 interface Locale {
+	function emptyDirectory(): String;
 	function error(): String;
 	function indexOf(directory: String): String;
+	function loading(): String;
 	function modifiedAt(): String;
 	function name(): String;
 	function parentDirectory(): String;
