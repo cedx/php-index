e63922065f6e1aa9d39d24f0418151fbc8c783fb
Cédric Belin <cedric@belin.io>
2022-06-26 00:27:43 +0200

    Ported the console application to JS

--- bin/php_index.js
+++ bin/php_index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-import {readFileSync} from "node:fs";
+import {randomUUID} from "node:crypto";
+import {cpSync, mkdirSync, readFileSync, rmSync} from "node:fs";
+import {tmpdir} from "node:os";
+import {join} from "node:path";
+import {fileURLToPath} from "node:url";
 import {program} from "commander";
+import {execaSync} from "execa";
 
 // Parse the command line arguments.
 const {version} = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf8"));
@@ -10,5 +15,13 @@ program.name("php_index")
 	.argument("<directory>", "the path to the output directory")
 	.parse();
 
-// Start the application.
-// TODO
+// Populate the input folder.
+const basePath = fileURLToPath(new URL("..", import.meta.url));
+const input = join(tmpdir(), randomUUID());
+["lib/server", "www"].forEach(folder => cpSync(join(basePath, folder), join(input, folder), {recursive: true}));
+["index.phar", "index.php"].forEach(file => rmSync(join(input, `www/${file}`), {force: true}));
+
+// Build the PHAR archive.
+const [output] = program.args;
+mkdirSync(output, {recursive: true});
+execaSync("php", [join(basePath, "bin/php_index.php"), "--input", input, "--output", output]);
