a6a9ab7304f01a4e86d3521a56ef4a0da6dba107
Cédric Belin <cedric@belin.io>
2022-06-28 10:55:13 +0200

    Updated the bundling settings

--- etc/rollup.js
+++ etc/rollup.js
@@ -2,14 +2,18 @@ import {env} from "node:process";
 import {nodeResolve} from "@rollup/plugin-node-resolve";
 import minifyHtmlLiterals from "rollup-plugin-minify-html-literals";
 
+// Value indicating whether the application runs in production mode.
+const production = env.NODE_ENV == "production";
+
 /** @type {import("rollup").RollupOptions} */
 export default {
+	treeshake: production,
 	input: "lib/client/index.js",
 	output: {
 		file: "www/js/main.js",
 		format: "es",
 	},
-	plugins: env.NODE_ENV == "production"
+	plugins: production
 		? [nodeResolve(), minifyHtmlLiterals()]
 		: [nodeResolve({exportConditions: ["development"]})]
 };
