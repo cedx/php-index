df8f07ee9ed2358402f6cbdddab04b8c8d263186
Cédric Belin <cedric@belin.io>
2022-08-08 17:05:12 +0200

    Code factorization

--- src/client/file_system/listing.js
+++ src/client/file_system/listing.js
@@ -1,10 +1,13 @@
 import {msg, str} from "@lit/localize";
 import {html} from "lit";
+import {classMap} from "lit/directives/class-map.js";
+import {choose} from "lit/directives/choose.js";
 import {when} from "lit/directives/when.js";
 import {Component} from "../component.js";
+import {LoadingStatus} from "../data/loading_status.js";
+import {Sort, SortOrder} from "../data/sort.js";
 import {getLocale} from "../locale.js";
-import {EntityList} from "./entity_list.js";
-import {EntityType} from "./entity.js";
+import {Entity, EntityType} from "./entity.js";
 
 /**
  * Displays the list of file system entities.
@@ -16,7 +19,9 @@ export class Listing extends Component {
 	 * @type {import("lit").PropertyDeclarations}
 	 */
 	static properties = {
-		entities: {state: true}
+		entities: {state: true},
+		loading: {state: true},
+		sort: {state: true}
 	};
 
 	/**
@@ -45,17 +50,83 @@ export class Listing extends Component {
 	#path = location.pathname.length > 1 ? location.pathname.slice(0, -1) : location.pathname;
 
 	/**
-	 * Creates a new entity list view.
+	 * Creates a new listing.
 	 */
 	constructor() {
 		super();
 
 		/**
 		 * The list of file system entities.
-		 * @type {EntityList}
-		 * @private
+		 * @type {Entity[]}
 		 */
-		this.entities = new EntityList;
+		this.entities = [];
+
+		/**
+		 * The loading status.
+		 * @type {LoadingStatus}
+		 */
+		this.loading = LoadingStatus.loading;
+
+		/**
+		 * The current sort.
+		 * @type {Sort}
+		 */
+		this.sort = new Sort;
+	}
+
+	/**
+	 * The view corresponding to the file listing.
+	 * @type {import("lit").TemplateResult}
+	 */
+	get listing() {
+		return html`
+			<table class="table table-hover table-sticky table-striped mb-0">
+				<thead>
+					<tr>
+						<th @click=${() => this.#orderBy("path")} scope="col">
+							<span role="button">${msg("Name")} <i class="bi bi-${this.sort.getIcon("path")}"></i></span>
+						</th>
+						<th class="text-end" @click=${() => this.#orderBy("size")} scope="col">
+							<span role="button">${msg("Size")} <i class="bi bi-${this.sort.getIcon("size")}"></i></span>
+						</th>
+						<th class="d-none d-sm-table-cell text-end" @click=${() => this.#orderBy("modifiedAt")} scope="col">
+							<span role="button">${msg("Last modified")} <i class="bi bi-${this.sort.getIcon("modifiedAt")}"></i></span>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					${when(this.#path.length > 1, () => html`
+						<tr>
+							<td colspan="2">
+								<div class="text-truncate">
+									<a href="..">
+										<i class="bi bi-arrow-90deg-up me-2"></i>${msg("Parent directory")}
+									</a>
+								</div>
+							</td>
+							<td class="d-none d-sm-table-cell"></td>
+						</tr>
+					`)}
+					${this.entities.map(entity => html`
+						<tr>
+							<td>
+								<div class="text-truncate">
+									<a href=${entity.type == EntityType.file ? entity.path : `${entity.path}/`}>
+										<i class="bi bi-${entity.icon} me-2"></i>${entity.path}
+									</a>
+								</div>
+							</td>
+							<td class="text-end">
+								${entity.type == EntityType.directory ? html`&ndash;` : this.#formatBytes(entity.size)}
+							</td>
+							<td class="d-none d-sm-table-cell text-end">
+								<time datetime=${entity.modifiedAt.toISOString()}>${this.#dateFormatter.format(entity.modifiedAt)}</time>
+							</td>
+						</tr>
+					`)}
+				</tbody>
+			</table>
+		`;
 	}
 
 	/**
@@ -64,7 +135,7 @@ export class Listing extends Component {
 	connectedCallback() {
 		super.connectedCallback();
 		document.title = `${location.hostname} - ${this.#path}`;
-		this.entities.fetch();
+		this.#fetch();
 	}
 
 	/**
@@ -74,58 +145,60 @@ export class Listing extends Component {
 	render() {
 		return html`
 			<article id="listing">
-				<section>
-					<h2>${msg(str`Index of ${this.#path}`)}</h2>
+				<section class=${classMap({"border-bottom": this.entities.length})}>
+					<h2 class="mb-0">${msg(str`Index of ${this.#path}`)}</h2>
 				</section>
 
-				<table class="table table-hover table-sticky table-striped">
-					<thead>
-						<tr>
-							<th @onclick=${() => this.entities.orderBy("path")} scope="col">
-								<span role="button">${msg("Name")} <i class="bi bi-${this.entities.sort.getIcon("path")}"></i></span>
-							</th>
-							<th class="text-end" @onclick=${() => this.entities.orderBy("size")} scope="col">
-								<span role="button">${msg("Size")} <i class="bi bi-${this.entities.sort.getIcon("size")}"></i></span>
-							</th>
-							<th class="d-none d-sm-table-cell text-end" @onclick=${() => this.entities.orderBy("modifiedAt")} scope="col">
-								<span role="button">${msg("Last modified")} <i class="bi bi-${this.entities.sort.getIcon("modifiedAt")}"></i></span>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						${when(this.#path.length > 1, () => html`
-							<tr>
-								<td colspan="2">
-									<div class="text-truncate">
-										<a href="..">
-											<i class="bi bi-arrow-90deg-up me-1"></i> ${msg("Parent directory")}
-										</a>
-									</div>
-								</td>
-								<td class="d-none d-sm-table-cell"></td>
-							</tr>
-						`)}
-						${this.entities.items.map(entity => html`
-							<tr>
-								<td>
-									<div class="text-truncate">
-										<a href=${entity.type == EntityType.file ? entity.path : `${entity.path}/`}>
-											<i class="bi bi-${entity.icon} me-1"></i> ${entity.path}
-										</a>
-									</div>
-								</td>
-								<td class="text-end">
-									${entity.type == EntityType.directory ? "&ndash;" : this.#formatBytes(entity.size)}
-								</td>
-								<td class="d-none d-sm-table-cell text-end">
-									<time datetime=${entity.modifiedAt.toISOString()}>${this.#dateFormatter.format(entity.modifiedAt)}</time>
-								</td>
-							</tr>
-						`)}
-					</tbody>
-				</table>
+				${choose(this.loading, [
+					[LoadingStatus.loading, () => html`
+						<section class="pt-0">
+							<div class="alert alert-info d-flex align-items-center mb-0">
+								<div class="spinner-border spinner-border-sm"></div>
+								<div class="ms-2">${msg("Loading the directory entries...")}</div>
+							</div>
+						</section>
+					`],
+					[LoadingStatus.failed, () => html`
+						<section class="pt-0">
+							<div class="alert alert-danger d-flex align-items-center mb-0">
+								<i class="bi bi-exclamation-circle-fill"></i>
+								<div class="ms-2">${msg("An error occurred while fetching the directory entries.")}</div>
+							</div>
+						</section>
+					`],
+					[LoadingStatus.done, () => this.entities.length ? this.listing : html`
+						<section class="pt-0">
+							<div class="alert alert-warning d-flex align-items-center mb-0">
+								<i class="bi bi-exclamation-triangle-fill"></i>
+								<div class="ms-2">${msg("This directory is empty.")}</div>
+							</div>
+						</section>
+					`]
+				])}
 			</article>
 		`;
+	}
+
+	/**
+	 * Fetches the list items.
+	 * @returns {Promise<void>} Resolves when the list items have been loaded.
+	 */
+	async #fetch() {
+		this.loading = LoadingStatus.loading;
+
+		try {
+			const response = await fetch("?listing");
+			if (!response.ok) this.loading = LoadingStatus.failed;
+			else {
+				const items = /** @type {import("./entity.js").EntityOptions[]} */ (await response.json());
+				this.entities = items.map(item => new Entity(item));
+				this.#orderBy("path");
+				this.loading = LoadingStatus.done;
+			}
+		}
+		catch {
+			this.loading = LoadingStatus.failed;
+		}
 	}
 
 	/**
@@ -141,6 +214,26 @@ export class Listing extends Component {
 		}
 
 		return this.#byteFormatter.format(bytes) + Listing.#byteUnits[index];
+	}
+
+	/**
+	 * Sorts the list items.
+	 * @param {string} attribute The sort attribute.
+	 * @param {SortOrder} [order] The sort order.
+	 */
+	#orderBy(attribute, order) {
+		if (!order) order = this.sort.get(attribute) == SortOrder.asc ? SortOrder.desc : SortOrder.asc;
+		this.sort = new Sort([[attribute, order]]);
+		this.entities = this.entities.sort((x, y) => {
+			switch (attribute) {
+				case "path": {
+					const value = x.type == y.type ? x.path.localeCompare(y.path) : x.type == EntityType.directory ? -1 : 1;
+					return order == SortOrder.asc ? value : -value;
+				}
+				default:
+					return this.sort.compare(x, y);
+			}
+		});
 	}
 }
 
