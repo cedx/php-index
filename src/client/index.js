14ba723a67126b5d769898e9b5982a964969489f
Cédric Belin <cedric@belin.io>
2022-10-03 19:48:31 +0200

    Reworked the spinner

--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,7 @@ import {locales, setLocale} from "./locale.js";
 // Load the components.
 import "./file_system/listing.js";
 import "./shell/navbar.js";
-import {Root} from "./shell/root.js";
+import "./shell/root.js";
 
 // Set the locale.
 const [locale] = navigator.language.split("-");
@@ -12,4 +12,4 @@ await setLocale(locales.has(locale) ? locale : document.documentElement.lang);
 
 // Start the application.
 window.fetch = fetchProxy;
-document.getElementById("loader")?.replaceWith(new Root);
+document.getElementById("loader")?.replaceWith(document.createElement("app-root"));
