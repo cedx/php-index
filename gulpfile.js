f46b8b2a3e9f15a1c6b14694f952d4b270962c5f
Cédric Belin <cedric@belin.io>
2023-01-17 00:18:18 +0100

    Updated the documentation [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ function buildTheme() {
 	return esbuild(cssOptions());
 }
 
-/** Deletes all generated files and reset any saved state. */
+/** Deletes all generated files. */
 export function clean() {
 	return deleteAsync(["src/client/i18n", "var/**/*", "www/*.phar", "www/css", "www/fonts", "www/js"]);
 }
@@ -53,7 +53,7 @@ export async function lint() {
 	return exec("tsc", ["--project", "jsconfig.json"]);
 }
 
-/** Publishes the package in the registry. */
+/** Publishes the package. */
 export async function publish() {
 	await exec("npm", ["publish"]);
 	for (const command of [["tag"], ["push", "origin"]]) await exec("git", [...command, `v${pkg.version}`]);
