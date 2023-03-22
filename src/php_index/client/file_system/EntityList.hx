ed541100b6757c1ce45b1b5935d73d26cbf7ff98
Cédric Belin <cedric@belin.io>
2023-03-22 21:38:18 +0100

    Add the skeleton of the listing component

---
+++ src/php_index/client/file_system/EntityList.hx
@@ -0,0 +1,45 @@
+package php_index.client.file_system;
+
+import php_index.base.data.Sort;
+import php_index.base.io.FileSystemEntity;
+
+/** Represents a list of file system entities. **/
+class EntityList implements Model {
+
+	/** The list items. **/
+	@:editable var items: List<FileSystemEntity> = new List();
+
+	/** The current sort. **/
+	@:editable var sort: Sort = new Sort();
+
+	/** The loading status. **/
+	/* TODO
+	@:loaded var status: List<FileSystemEntity> = Application.instance.remote.index({listing: true}).next(list -> {
+		items = list;
+		orderBy("path");
+		items;
+	}); */
+
+	/** Sorts the list of file system entities. **/
+	/* TODO
+	public function orderBy(attribute: String, ?direction: SortDirection) {
+		if (direction == null) direction = if (sort.exists(attribute)) sort[attribute] == Asc ? Desc : Asc else Asc;
+		sort = [attribute => direction];
+		items = items.sort((x, y) -> {
+			final field1 = Reflect.getProperty(x, attribute);
+			final field2 = Reflect.getProperty(y, attribute);
+			final value = switch attribute {
+				case "modifiedAt":
+					Reflect.compare((field1: Date).getTime(), (field2: Date).getTime());
+				case "path":
+					final areDirectories = x.type == Directory && y.type == Directory;
+					final areFiles = x.type == File && y.type == File;
+					if (areDirectories || areFiles) Reflect.compare(field1, field2) else x.type == Directory ? -1 : 1;
+				default:
+					Reflect.compare(field1, field2);
+			}
+
+			direction == Asc ? value : -value;
+		});
+	}*/
+}
--- src/php_index/client/file_system/Listing.hx
+++ src/php_index/client/file_system/Listing.hx
@@ -1,1 +1,10 @@
 package php_index.client.file_system;
+
+/** Displays the list of file system entities. **/
+class Listing extends View {
+
+	/** Renders this view. **/
+	function render() '
+		<article id="listing"></article>
+	';
+}
