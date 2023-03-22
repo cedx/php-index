af1f438fb677839b8cddb84fbb92493c324770f3
Cédric Belin <cedric@belin.io>
2023-03-22 19:11:34 +0100

    Update the PHAR stub

--- bin/php_index.php
+++ bin/php_index.php
@@ -3,22 +3,24 @@
 $stub = <<<'EOF'
 <?php
 $rootPath = basename(__FILE__);
-require "phar://$rootPath/src/php_index/server/index.php";
-\php_index\main();
+spl_autoload_register(fn($class) => include "phar://$rootPath/lib/".str_replace("\\", "/", $class).".php");
+\php\Boot::__hx__init();
+\php_index\server\_Program\Program_Fields_::main();
+\haxe\EntryPoint::run();
 __HALT_COMPILER();
 EOF;
 
 try {
 	// Check the requirements.
-	if (!extension_loaded("phar")) throw new RuntimeException("Phar extension is not loaded.", 1);
-	if (!Phar::canWrite()) throw new RuntimeException("Phar extension does not support creating Phar archives.", 2);
+	if (!extension_loaded("phar")) throw new RuntimeException("Phar extension is not loaded.", 501);
+	if (!Phar::canWrite()) throw new RuntimeException("Phar extension does not support creating Phar archives.", 501);
 
 	// Parse the command line arguments.
 	$options = getopt("i:o:", ["input:", "output:"]) ?: [];
 	$input = $options["i"] ?? ($options["input"] ?? null);
-	if (!$input || !is_dir($input)) throw new RuntimeException("You must provide a valid path to the input directory.", 3);
+	if (!$input || !is_dir($input)) throw new RuntimeException("You must provide a valid path to the input directory.", 400);
 	$output = $options["o"] ?? ($options["output"] ?? null);
-	if (!$output || !is_dir($output)) throw new RuntimeException("You must provide a valid path to the output directory.", 4);
+	if (!$output || !is_dir($output)) throw new RuntimeException("You must provide a valid path to the output directory.", 400);
 
 	// Create the PHAR archive.
 	$phar = new Phar("$output/index.phar");
@@ -27,6 +29,7 @@ try {
 	exit(0);
 }
 catch (Throwable $e) {
+	$code = $e->getCode();
 	echo $e->getMessage(), PHP_EOL;
-	exit($e->getCode() ?: 5);
+	exit($code >= 400 && $code < 600 ? $code : 500);
 }
