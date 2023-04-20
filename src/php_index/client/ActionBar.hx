3906b9583747a5e0a4ac033b62ddf1d1fa9a0969
Cédric Belin <cedric@belin.io>
2023-04-20 15:52:14 +0200

    Add the `ActionBar` component

---
+++ src/php_index/client/ActionBar.hx
@@ -0,0 +1,29 @@
+package php_index.client;
+
+import js.Browser;
+import js.html.Element;
+
+/** An action bar located under the navigation bar. **/
+class ActionBar extends View {
+
+	/** The view children. **/
+	@:children var elements: Children;
+
+	/** The root element. **/
+	@:ref final root: Element;
+
+	/** Renders this view. **/
+	function render() '
+		<aside class="container-fluid" ref=${root}>
+			<div class="d-flex justify-content-between align-items-center">${...elements}</div>
+		</aside>
+	';
+
+	/** Method invoked after this view is mounted. **/
+	override function viewDidMount() {
+		final navbarHeight = Std.parseInt(Browser.window.getComputedStyle(Browser.document.documentElement).getPropertyValue("--navbar-height"));
+		final style = Browser.document.documentElement.style;
+		style.setProperty("--main-offset", '${navbarHeight + root.offsetHeight}px');
+		beforeUnmounting(() -> style.removeProperty("--main-offset"));
+	}
+}
--- src/php_index/client/import.hx
+++ src/php_index/client/import.hx
@@ -1,4 +1,5 @@
 import coconut.data.List;
 import coconut.data.Model;
+import coconut.ui.Children;
 import coconut.ui.View;
 using tink.CoreApi;
