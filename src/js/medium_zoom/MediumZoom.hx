37c3c7483e8ddc661265250eee9d057a860910c3
Cédric Belin <cedric@belin.io>
2023-04-23 19:05:20 +0200

    Update the externs

--- src/js/medium_zoom/MediumZoom.hx
+++ src/js/medium_zoom/MediumZoom.hx
@@ -1,9 +1,13 @@
 package js.medium_zoom;
 
+import haxe.Constraints.Function;
 import haxe.extern.EitherType;
+import js.html.AddEventListenerOptions;
 import js.html.Element;
+import js.html.EventListener;
 import js.html.NodeList;
 import js.html.TemplateElement;
+import js.lib.Promise;
 
 /** Provides a zoom effect for a selection of images. **/
 @:native("mediumZoom")
@@ -11,8 +15,48 @@ extern class MediumZoom {
 
 	/** Attaches a zoom effect to a selection of images. **/
 	@:selfCall
-	@:overload(function(selector: ZoomSelector, ?options: ZoomOptions): Void {})
-	static function zoom(?options: ZoomOptions): Void;
+	@:overload(function(selector: ZoomSelector, ?options: ZoomOptions): Zoom {})
+	static function zoom(?options: ZoomOptions): Zoom;
+}
+
+/** Handles the zoom effect. **/
+extern class Zoom {
+
+	/** Attaches the images to the zoom. **/
+	function attach(...selectors: ZoomSelector): Zoom;
+
+	/** Extends the zoom with the provided options merged with the current ones. **/
+	function clone(?options: ZoomOptions): Zoom;
+
+	/** Closes the zoom. **/
+	function close(): Promise<Zoom>;
+
+	/** Releases the images from the zoom. **/
+	function detach(...selectors: ZoomSelector): Zoom;
+
+	/** Returns the zoom images. **/
+	function getImages(): Array<Element>;
+
+	/** Returns the zoom options. **/
+	function getOptions(): ZoomOptions;
+
+	/** Returns the current zoomed image. **/
+	function getZoomedImage(): Element;
+
+	/** Unregisters an event handler. **/
+	function off(type: ZoomEvent, listener: EitherType<Function, EventListener>, ?options: EitherType<Bool, AddEventListenerOptions>): Zoom;
+
+	/** Opens the zoom. **/
+	function open(?options: ZoomOpenOptions): Promise<Zoom>;
+
+	/** Registers an event handler on each target of the zoom. **/
+	function on(type: ZoomEvent, listener: EitherType<Function, EventListener>, ?options: EitherType<Bool, AddEventListenerOptions>): Zoom;
+
+	/** Toggles the zoom. **/
+	function toggle(?options: ZoomOpenOptions): Promise<Zoom>;
+
+	/** Updates the options. **/
+	function update(options: ZoomOptions): Zoom;
 }
 
 /** Defines the position and size of the zoom container. **/
@@ -23,6 +67,35 @@ typedef ZoomContainer = {
 	var ?right: Int;
 	var ?top: Int;
 	var ?width: Int;
+}
+
+/** Enumeration of events emitted by the zoom. **/
+enum abstract ZoomEvent(String) to String {
+
+	/** Fired immediately when the close method is called. **/
+	var Close = "close";
+
+	/** Fired when the zoom out has finished being animated. **/
+	var Closed = "closed";
+
+	/** Fired when the detach method is called. **/
+	var Detach = "detach";
+
+	/** Fired immediately when the open method is called. **/
+	var Open = "open";
+
+	/** Fired when the zoom has finished being animated. **/
+	var Opened = "opened";
+
+	/** Fired when the update method is called. **/
+	var Update = "update";
+}
+
+/** Defines the options of the `Zoom.open()` method. **/
+typedef ZoomOpenOptions = {
+
+	/** The target of the zoom. **/
+	var ?target: Element;
 }
 
 /** Defines the options of the `MediumZoom.zoom()` method. **/
