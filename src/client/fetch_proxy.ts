be0c4685816e31fa27c7b754ac4281c621ba8d67
Cédric Belin <cedric@belin.io>
2024-01-27 03:55:07 +0100

    Add a `fetch` proxy

---
+++ src/client/fetch_proxy.ts
@@ -0,0 +1,27 @@
+/**
+ * Fetches a resource from the network, returning a promise which is fulfilled once the response is available.
+ */
+export const fetchProxy = new Proxy(fetch, {
+
+	/**
+	 * A trap for a `fetch()` call.
+	 * @param target The target callable object.
+	 * @param thisArg The `this` argument for the call.
+	 * @param args The list of arguments for the call.
+	 * @returns The server response.
+	 */
+	async apply(target: typeof fetch, thisArg: unknown, args: [RequestInfo|URL, RequestInit|undefined]): Promise<Response> {
+		const request = new Request(...args);
+		if (!request.headers.has("Accept")) request.headers.set("Accept", "application/json");
+
+		const methods = ["PATCH", "POST", "PUT"];
+		if (methods.includes(request.method)) {
+			const [mimeType] = (request.headers.get("Content-Type") ?? "").split(";");
+			if (!mimeType || mimeType == "text/plain") request.headers.set("Content-Type", "application/json");
+		}
+
+		const response = await Reflect.apply(target, thisArg, [request]);
+		if (!response.ok) throw Object.assign(new Error(`${response.status} ${response.statusText}`), {name: "HttpError"});
+		return response;
+	}
+});
--- src/php_index/client/import.hx
+++
@@ -1,5 +0,0 @@
-import coconut.data.List;
-import coconut.data.Model;
-import coconut.ui.Children;
-import coconut.ui.View;
-using tink.CoreApi;
