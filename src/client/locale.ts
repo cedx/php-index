eaf9beb3741bda2401a172105f565fdd7d46cb09
Cédric Belin <cedric@belin.io>
2024-01-27 13:54:36 +0100

    Enable the localization

--- src/client/locale.ts
+++ src/client/locale.ts
@@ -1,11 +1,11 @@
 import {configureLocalization, type LocaleModule} from "@lit/localize";
-// TODO import * as french from "./i18n/fr.js";
+import * as french from "./i18n/fr.js";
 
 /**
  * The map of supported locales.
  */
 export const locales = new Map<string, LocaleModule>([
-	// TODO ["fr", french]
+	["fr", french]
 ]);
 
 /**
