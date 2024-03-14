6899cefe4b17e96ccaf25376073aa7abf4d97a86
Cédric Belin <cedric@belin.io>
2024-03-15 00:47:18 +0100

    Update the dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ export function i18n() {
 
 // Performs the static analysis of source code.
 export async function lint() {
-	await $`tsc --project tsconfig.json`;
+	await $`tsc --project .`;
 	await $`eslint --config=etc/eslint.config.js gulpfile.js etc src`;
 	await $`stylelint --config=etc/stylelint.js src/ui/**/*.scss`;
 	return $`vendor/bin/phpstan analyse --configuration=etc/phpstan.php --memory-limit=256M`;
