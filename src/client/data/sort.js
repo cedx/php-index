da4edae68a61de856cc9d1da0308858337af3c40
Cédric Belin <cedric@belin.io>
2024-04-28 17:07:17 +0200

    Port the `Sort` class

--- src/client/data/sort.js
+++ src/client/data/sort.js
@@ -1,23 +1,24 @@
 /**
  * Represents an attribute/order pair of a sort.
+ * @typedef {[string, SortOrder]} SortEntry
  */
-export type SortEntry = [string, SortOrder];
 
 /**
  * Specifies the order of a sort parameter.
+ * @enum {string}
  */
-export enum SortOrder {
+export const SortOrder = Object.freeze({
 
 	/**
 	 * The sort is ascending.
 	 */
-	asc = "ASC",
+	asc: "ASC",
 
 	/**
 	 * The sort is descending.
 	 */
-	desc = "DESC"
-}
+	desc: "DESC"
+});
 
 /**
  * Represents information relevant to the sorting of data items.
@@ -26,30 +27,32 @@ export class Sort {
 
 	/**
 	 * The list of attribute/order pairs.
+	 * @type {SortEntry[]}
 	 */
-	#attributes: SortEntry[];
+	#attributes;
 
 	/**
 	 * Creates new sort.
-	 * @param attributes The list of attributes to be sorted.
+	 * @param {SortEntry[]} attributes The list of attributes to be sorted.
 	 */
-	constructor(attributes: SortEntry[] = []) {
+	constructor(attributes = []) {
 		this.#attributes = attributes;
 	}
 
 	/**
 	 * The number of attributes.
+	 * @type {number}
 	 */
-	get length(): number {
+	get length() {
 		return this.#attributes.length;
 	}
 
 	/**
 	 * Creates a new sort from the specified string.
-	 * @param value A string representing a sort.
-	 * @returns The sort corresponding to the specified string.
+	 * @param {string} value A string representing a sort.
+	 * @returns {Sort} The sort corresponding to the specified string.
 	 */
-	static parse(value: string): Sort {
+	static parse(value) {
 		return new this((value ? value.split(",") : []).map(item => {
 			const order = item.startsWith("-") ? SortOrder.desc : SortOrder.asc;
 			return [order == SortOrder.asc ? item : item.slice(1), order];
@@ -58,19 +61,19 @@ export class Sort {
 
 	/**
 	 * Returns a new iterator that allows iterating the entries of this sort.
-	 * @returns An iterator over the attribute/order pairs.
+	 * @returns {IterableIterator<SortEntry>} An iterator over the attribute/order pairs.
 	 */
-	*[Symbol.iterator](): IterableIterator<SortEntry> {
+	*[Symbol.iterator]() {
 		for (const entry of this.#attributes) yield entry;
 	}
 
 	/**
 	 * Appends the specified attribute to this sort.
-	 * @param attribute The attribute name.
-	 * @param order The sort order.
-	 * @returns This instance.
+	 * @param {string} attribute The attribute name.
+	 * @param {SortOrder} order The sort order.
+	 * @returns {this} This instance.
 	 */
-	append(attribute: string, order: SortOrder): this {
+	append(attribute, order) {
 		this.delete(attribute);
 		this.#attributes.push([attribute, order]);
 		return this;
@@ -78,23 +81,23 @@ export class Sort {
 
 	/**
 	 * Gets the attribute/order pair at the specified index.
-	 * @param index The position in this sort.
-	 * @returns The attribute/order pair at the specified index, or `null` if it doesn't exist.
+	 * @param {number} index The position in this sort.
+	 * @returns {SortEntry|null} The attribute/order pair at the specified index, or `null` if it doesn't exist.
 	 */
-	at(index: number): SortEntry|null {
+	at(index) {
 		return this.#attributes.at(index) ?? null;
 	}
 
 	/**
 	 * Compares the specified objects, according to the current sort attributes.
-	 * @param x The first object to compare.
-	 * @param y The second object to compare.
-	 * @returns A value indicating the relationship between the two objects.
+	 * @param {object} x The first object to compare.
+	 * @param {object} y The second object to compare.
+	 * @returns {number} A value indicating the relationship between the two objects.
 	 */
-	compare(x: object, y: object): number {
+	compare(x, y) {
 		for (const [attribute, order] of this.#attributes) {
-			const xAttr = Reflect.get(x, attribute); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
-			const yAttr = Reflect.get(y, attribute); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+			const xAttr = Reflect.get(x, attribute);
+			const yAttr = Reflect.get(y, attribute);
 			const value = xAttr > yAttr ? 1 : (xAttr < yAttr ? -1 : 0);
 			if (value) return order == SortOrder.asc ? value : -value;
 		}
@@ -104,28 +107,28 @@ export class Sort {
 
 	/**
 	 * Removes the specified attribute from this sort.
-	 * @param attribute The attribute name.
+	 * @param {string} attribute The attribute name.
 	 */
-	delete(attribute: string): void {
+	delete(attribute) {
 		this.#attributes = this.#attributes.filter(([key]) => key != attribute);
 	}
 
 	/**
 	 * Gets the order associated with the specified attribute.
-	 * @param attribute The attribute name.
-	 * @returns The order associated with the specified attribute, or `null` if the attribute doesn't exist.
+	 * @param {string} attribute The attribute name.
+	 * @returns {SortOrder|null} The order associated with the specified attribute, or `null` if the attribute doesn't exist.
 	 */
-	get(attribute: string): SortOrder|null {
+	get(attribute) {
 		for (const [key, order] of this.#attributes) if (key == attribute) return order;
 		return null;
 	}
 
 	/**
 	 * Gets the icon corresponding to the specified attribute.
-	 * @param attribute The attribute name.
-	 * @returns The icon corresponding to the specified attribute.
+	 * @param {string} attribute The attribute name.
+	 * @returns {string} The icon corresponding to the specified attribute.
 	 */
-	getIcon(attribute: string): string {
+	getIcon(attribute) {
 		switch (this.get(attribute)) {
 			case SortOrder.asc: return "arrow_upward";
 			case SortOrder.desc: return "arrow_downward";
@@ -135,30 +138,30 @@ export class Sort {
 
 	/**
 	 * Returns a value indicating whether the specified attribute exists in this sort.
-	 * @param attribute The attribute name.
-	 * @returns `true` if the specified attribute exists in this sort, otherwise `false`.
+	 * @param {string} attribute The attribute name.
+	 * @returns {boolean} `true` if the specified attribute exists in this sort, otherwise `false`.
 	 */
-	has(attribute: string): boolean {
+	has(attribute) {
 		return this.#attributes.some(([key]) => key == attribute);
 	}
 
 	/**
 	 * Gets the index of the specified attribute in the underlying list.
-	 * @param attribute The attribute name.
-	 * @returns The index of the specified attribute, or `-1` if the attribute is not found.
+	 * @param {string} attribute The attribute name.
+	 * @returns {number} The index of the specified attribute, or `-1` if the attribute is not found.
 	 */
-	indexOf(attribute: string): number {
+	indexOf(attribute) {
 		for (const [index, [key]] of this.#attributes.entries()) if (key == attribute) return index;
 		return -1;
 	}
 
 	/**
 	 * Prepends the specified attribute to this sort.
-	 * @param attribute The attribute name.
-	 * @param order The sort order.
-	 * @returns This instance.
+	 * @param {string} attribute The attribute name.
+	 * @param {SortOrder} order The sort order.
+	 * @returns {this} This instance.
 	 */
-	prepend(attribute: string, order: SortOrder): this {
+	prepend(attribute, order) {
 		this.delete(attribute);
 		this.#attributes.unshift([attribute, order]);
 		return this;
@@ -166,10 +169,10 @@ export class Sort {
 
 	/**
 	 * Returns a value indicating whether the current sort satisfies the specified conditions.
-	 * @param conditions The conditions to satisfy.
-	 * @returns `true` if the current sort satisfies the specified conditions, otherwise `false`.
+	 * @param {Partial<{attributes: string[], min: number, max: number}>} conditions The conditions to satisfy.
+	 * @returns {boolean} `true` if the current sort satisfies the specified conditions, otherwise `false`.
 	 */
-	satisfies(conditions: Partial<{attributes: string[], min: number, max: number}> = {}): boolean {
+	satisfies(conditions = {}) {
 		const min = conditions.min ?? -1;
 		if (min >= 0) return this.length >= min;
 
@@ -182,11 +185,11 @@ export class Sort {
 
 	/**
 	 * Sets the order of the specified attribute.
-	 * @param attribute The attribute name.
-	 * @param order The sort order.
-	 * @returns This instance.
+	 * @param {string} attribute The attribute name.
+	 * @param {SortOrder} order The sort order.
+	 * @returns {this} This instance.
 	 */
-	set(attribute: string, order: SortOrder): this {
+	set(attribute, order) {
 		for (const [index, [key]] of this.#attributes.entries()) if (key == attribute) {
 			this.#attributes[index] = [key, order];
 			return this;
@@ -197,26 +200,26 @@ export class Sort {
 
 	/**
 	 * Converts this object to a string in JSON format.
-	 * @returns The string in JSON format corresponding to this object.
+	 * @returns {string} The string in JSON format corresponding to this object.
 	 */
-	toJSON(): string {
+	toJSON() {
 		return this.toString();
 	}
 
 	/**
 	 * Converts this sort to an SQL clause.
-	 * @param escape A function used to escape the SQL identifiers.
-	 * @returns The SQL clause corresponding to this object.
+	 * @param {(identifier: string) => string} [escape] A function used to escape the SQL identifiers.
+	 * @returns {string} The SQL clause corresponding to this object.
 	 */
-	toSql(escape?: (identifier: string) => string): string {
+	toSql(escape) {
 		return this.#attributes.map(([attribute, order]) => `${escape ? escape(attribute) : attribute} ${order}`).join(", ");
 	}
 
 	/**
 	 * Returns a string representation of this object.
-	 * @returns The string representation of this object.
+	 * @returns {string} The string representation of this object.
 	 */
-	toString(): string {
+	toString() {
 		return this.#attributes.map(([attribute, order]) => `${order == SortOrder.desc ? "-" : ""}${attribute}`).join();
 	}
 }
