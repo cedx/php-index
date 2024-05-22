bb4cda098050f45fb290857e8069ce2d6da5a59f
Cédric Belin <cedric@belin.io>
2024-05-22 09:05:27 +0200

    Update the JSON serialization [skip ci]

--- src/server/io/FileSystemEntity.php
+++ src/server/io/FileSystemEntity.php
@@ -36,9 +36,9 @@ final readonly class FileSystemEntity implements \JsonSerializable {
 
 	/**
 	 * Converts this object to a map in JSON format.
-	 * @return object The map in JSON format corresponding to this object.
+	 * @return \stdClass The map in JSON format corresponding to this object.
 	 */
-	function jsonSerialize(): object {
+	function jsonSerialize(): \stdClass {
 		return (object) [
 			"modifiedAt" => $this->modifiedAt()->format("c"),
 			"path" => basename($this->path),
